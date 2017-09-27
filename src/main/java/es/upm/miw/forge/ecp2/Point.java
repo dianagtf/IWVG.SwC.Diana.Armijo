fb8f0fcc70c3322e7382a3c6571bedeca0ad9fbc
maste <maste@LAPTOP-9Q7VHT04>
2017-09-27 19:28:49 +0200

    Modify Point add z

--- src/main/java/es/upm/miw/forge/ecp2/Point.java
+++ src/main/java/es/upm/miw/forge/ecp2/Point.java
@@ -1,19 +1,20 @@
 package es.upm.miw.forge.ecp2;
 
 public class Point {
-    private int x, y;
+    private int x, y, z;
 
-    public Point(int x, int y) {
+    public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
+        this.z = z;      
     }
 
-    public Point(int xy) {
-        this(xy, xy);
+    public Point(int xyz) {
+        this(xyz, xyz, xyz);
     }
 
     public Point() {
-        this(0, 0);
+        this(0, 0, 0);
     }
 
     public int getX() {
@@ -24,8 +25,12 @@ public class Point {
         return this.y;
     }
 
+    public int getZ() {
+        return this.z;
+    }
+    
     public double module() {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
     public double phase() {
@@ -35,11 +40,12 @@ public class Point {
     public void translateOrigin(Point origin) {
         this.x -= origin.getX();
         this.y -= origin.getY();
+        this.z -= origin.getZ();
     }
 
     @Override
     public String toString() {
-        return "Point[" + x + "," + y + "]";
+        return "Point[" + x + "," + y + "," + z + "]";
     }
     
 }
